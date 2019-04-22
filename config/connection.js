0ff19f617cb97cfdd1609a84bb535fabdcf087e0
Ashell Fernando <ashellz2010@gmail.com>
2019-04-22 18:21:48 -0400

    added db connection, added db keys

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+const keys = require('./keys');
+const connection = mysql.createConnection({
+  host: keys.mysqlHost,
+  port: 3306,
+  user: keys.mysqlUser,
+  password: keys.mysqlPass,
+  database: keys.mysqlDB
+});
+
+connection.connect(err => {
+  if (err) {
+    console.error(`Error Connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`Connected to database succesfully`);
+});
+
+module.exports = connection;
---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+  JAWSDB_URI: process.env.JAWSDB_URI
+};
