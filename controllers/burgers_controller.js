6d9985814482d7a1d1002b59cae92db87a58edcf
Ashell Fernando <ashellz2010@gmail.com>
2019-04-22 19:45:12 -0400

    added handlebars main file, with jquery& bootstrap

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+const burger = require('../models/burger');
+
+router.get('/', (req, res) => {
+  burger.selectAll(data => {
+    // let testObj = {
+    //   burgers: data
+    // };
+    // console.log(testObj);
+    // res.render('index', testObj);
+    res.render('index', { data });
+  });
+});
+
+router.post('/addBurger', (req, res) => {
+  burger.create(req.body.new_burger, result => {
+    console.log(result);
+  });
+});
+
+router.put('/update', (req, res) => {
+  burger.updateOne(req.body.id, result => {
+    console.log(result);
+  });
+});
+
+module.exports = router;
