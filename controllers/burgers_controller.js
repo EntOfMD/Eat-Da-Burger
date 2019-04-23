4b174b92fc42ed510af7aba197af4c71a1c9af06
Ashell Fernando <ashellz2010@gmail.com>
2019-04-23 09:33:04 -0400

    routes work as intended now

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,24 +5,25 @@ const burger = require('../models/burger');
 
 router.get('/', (req, res) => {
   burger.selectAll(data => {
-    // let testObj = {
-    //   burgers: data }
-    // console.log(testObj);
-    // res.render('index', testObj);
     res.render('index', { data });
   });
 });
 
 router.post('/addBurger', (req, res) => {
-  burger.create(req.body.new_burger, result => {
+  burger.insertOne(req.body.bg_name, result => {
     console.log(result);
   });
 });
 
-router.put('/update', (req, res) => {
-  burger.updateOne(req.body.id, result => {
-    console.log(result);
-  });
+router.post('/update', (req, res) => {
+  if (req.body) {
+    console.log(req.body);
+    burger.updateOne(req.body.id, result => {
+      console.log(result);
+    });
+  } else {
+    console.log(`payload not found!`);
+  }
 });
 
 module.exports = router;
