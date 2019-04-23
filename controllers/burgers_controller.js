f4bbd77bddbc8ba1a369a5e806cd16782b2d7365
Ashell Fernando <ashellz2010@gmail.com>
2019-04-23 10:22:58 -0400

    it now redirects because page won't reload itself

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,23 +3,32 @@ const router = express.Router();
 
 const burger = require('../models/burger');
 
+//the page doesn't reload once, so we're forcing it to
 router.get('/', (req, res) => {
+  res.redirect('/app');
+});
+
+router.get('/app', (req, res) => {
   burger.selectAll(data => {
     res.render('index', { data });
   });
 });
 
-router.post('/addBurger', (req, res) => {
-  burger.insertOne(req.body.bg_name, result => {
-    console.log(result);
+router.post('/app/addBurger', (req, res) => {
+  burger.insertOne(req.body.burger_name, result => {
+    if (result) {
+      console.log(`added, redirecting`);
+      res.redirect('/');
+    }
   });
 });
 
-router.post('/update', (req, res) => {
+//router.put didn't work for me, but this did
+router.put('/app/update', (req, res) => {
   if (req.body) {
-    console.log(req.body);
     burger.updateOne(req.body.id, result => {
-      console.log(result);
+      console.log(`redirecting`);
+      res.redirect('/');
     });
   } else {
     console.log(`payload not found!`);
